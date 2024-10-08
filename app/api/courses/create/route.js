e589fa903c4b943730ab50ee619164285ecd4a6e
rowan <u6511498@au.edu>
2024-10-08 19:42:42 +0700

    gpa

---
+++ app/api/courses/create/route.js
@@ -0,0 +1,32 @@
+import dbConnect from "@/lib/db";
+import Course from "@/models/Course";
+
+export async function POST(req) {
+  await dbConnect();
+
+  const { name, grade, credits } = await req.json();
+  console.log(name);
+
+  // Validate input
+  if (!name || !grade || credits < 0) {
+    return new Response(JSON.stringify({ message: "Invalid course data" }), {
+      status: 400,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  const newCourse = new Course({ name, grade, credits });
+
+  try {
+    const savedCourse = await newCourse.save();
+    return new Response(JSON.stringify(savedCourse), {
+      status: 201,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ message: "Error creating course" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
